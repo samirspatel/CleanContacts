35811f016e520e5c9ab0860e1b6b4a63f7ff30a7
samir <me@samirpatel.me>
2025-01-04 23:09:46 -0600

    fix:commit

--- CleanContacts/CleanContactsApp.swift
+++ CleanContacts/CleanContactsApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct CleanContactsApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Contact.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
